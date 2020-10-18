d967e8f1ec6b9b4b78326270742ac6db9334e9bf
Vy <remy_q7@yahoo.com>
2020-10-18 14:57:31 -0400

    update js

--- src/app.js
+++ src/app.js
@@ -42,13 +42,6 @@ function displayWeatherCondition(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
-function showFahrenheitTemperature(event) {
-  event.preventDefault();
-  let fahrenheitTemperature = ((14 * 9) / 5) * 32;
-
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = fahrenheitTemperature;
-}
 
 function searchCity(city) {
   let apiKey = "df00ce6442112c0f15afa927a09d9e5a";
