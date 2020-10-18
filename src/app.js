5c17e171fb52b31c862ac9dbc2dd244c0e4dc943
Vy <remy_q7@yahoo.com>
2020-10-18 14:34:56 -0400

    update js

--- src/app.js
+++ src/app.js
@@ -36,11 +36,6 @@ function displayWeatherCondition(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
-  let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
 }
 function searchCity(city) {
   let apiKey = "df00ce6442112c0f15afa927a09d9e5a";
@@ -63,6 +58,11 @@ function searchLocation(position) {
 }
 
 celciusTemperature = response.data.main.temp;
+let iconElement = document.querySelector("#icon");
+iconElement.setAttribute(
+  "src",
+  `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+);
 
 let temperatureElement = document.querySelector("#temperature");
 temperatureElement.innerHTML = fahrenheitTemperature;
