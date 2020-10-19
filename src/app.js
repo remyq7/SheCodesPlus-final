b23d8c0664239457347d699e0ef3fb7e119e349a
Vy <remy_q7@yahoo.com>
2020-10-18 22:41:57 -0400

    update js

--- src/app.js
+++ src/app.js
@@ -98,10 +98,5 @@ function searchLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
+
 searchCity("New York");
